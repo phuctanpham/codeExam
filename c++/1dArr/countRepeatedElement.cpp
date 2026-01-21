bd36cd3b13642ff90bf3feadd21da6577e9c8318
phuctanpham <phuctanpham@gmail.com>
2026-01-21 04:34:29 +0000

    phucpt

--- c++/1dArr/countRepeatedElement.cpp
+++ c++/1dArr/countRepeatedElement.cpp
@@ -1,30 +1,46 @@
 #include <iostream>
 using namespace std;
-
-/* 
-input:
- + float a[]
- + int n
- + int element
-Ouput:
- + return int count
-*/
-
-float countElement(float a[], int n, int element, int& count){
-    for(int i = 0; i < n; i++){
-        if (a[i] == element){
+float newArr[10];
+int m = 0;
+// Checks if a value already exists in the simplified array
+bool toDuplicate(float arr[], int n, float checkValue) {
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == checkValue) {
+            return true;
+        }
+    }
+    return false;
+}
+// Fills newArr with unique elements from arr
+void simplifyArr(float arr[], int n, int& m) {
+    for(int i = 0; i < n; i++) {
+        if(!(toDuplicate(newArr, m, arr[i]))) {
+            newArr[m] = arr[i];
+            m++;
+        }
+    }
+}
+// Counts how many times an element appears in the original array
+int countElement(float arr[], int n, float element) {
+    int count = 0;
+    for(int i = 0; i < n; i++) {
+        if (arr[i] == element) {
             count++;
         }
     }
     return count;
 }
 
-int main(){
-    int count = 0;
-    float initArr[] = {1, 1.1, 2, 2.2, 3, 4, 2, 1, 3, 4, 3, 4, 1, 4, 3};
+// Print result
+int main() {
+    float Arr[] = {1, 1.1, 2, 2.2, 3, 4, 2, 1, 3, 4, 3, 4, 1, 4, 3};
     int n = 15;
-    int element = 4;
-    countElement(initArr, n, element, count);
-    cout << "There are " << count << " of number " << element << endl;
+    simplifyArr(Arr, n, m);
+    for(int i = 0; i < m; i++) {
+        float element = newArr[i];
+        int count = countElement(Arr, n, element);
+        cout << i + 1 << ") There are " << count << " of number " << element << endl;
+    }
+
     return 0;
 }
